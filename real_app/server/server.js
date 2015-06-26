58c984387a746775f4dc44c5a870b8e53b01c050
Duc <trananhduc1004@gmail.com>
2015-06-25 22:16:25 -0400

    Separate and hook up client and server side, generate jquery, angular and uirouter

--- real_app/server/server.js
+++ real_app/server/server.js
@@ -3,11 +3,16 @@ var express = require('express');// Web app framework that provides simpler inte
 var path = require('path'); // Has helper function to handle path manipulation
 //
 var app = express(); // Create a server object
-app.get('/', function(req, res) {
-	res.send('YOLO BIATCHES');
-});
+// app.get('/', function(req, res) {
+// 	res.send('YOLO BIATCHES');
+// }); // Route for home page.
 var server = app.listen(6969, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Club life happening at http://%s:%s', host, port);
 }); // Starts the server
+// Serve the '../client' folder directly to the server.
+app.use('/', express.static('../client'));
+// app.use:  Mount the middleware function to the path. Default to '/'
+// Note: if there is an index.html file, it will be bounded directly to '/' (the home path)
+// 'partials folder': Where to keep the Front end (angularjs) single view.
