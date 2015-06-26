dbe627db48bc043fb55d527f7993c75b52f03f13
Duc <trananhduc1004@gmail.com>
2015-06-25 21:11:42 -0400

    Generate an angular fullstack app

---
+++ sample_app/client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('sampleAppApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
