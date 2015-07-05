7dd994aef609d0075b5d272bd753de0af82df48a
Christopher <ckemeza1@gmail.com>
2015-07-05 14:09:14 -0400

    fixed some styling issues

--- real_app/server/api/deck/deck.model.js
+++ real_app/server/api/deck/deck.model.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose'); // Get mongoose ORM
 var deckSchema = new mongoose.Schema({
 	name: String,
-  description: String,
-  cards: Array,
-  userId: String,
-  pic: String
-});
+  	description: String,
+  	cards: Array,
+  	userId: String,
+  	pic: String
+	});
 var Deck = mongoose.model('Deck', deckSchema);
 module.exports = Deck; // Create and export Deck model as an object
 
